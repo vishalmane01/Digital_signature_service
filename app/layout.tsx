896bcd5cefe916e9d16b4ed8e7a301c6e77eee54
Vishal <vishal012mane@gmail.com>
2025-10-16 22:47:11 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Suspense } from "react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "DIgitalSignatureService",
+  description: "Project",
+  generator: "Vishal",
 }
 
 export default function RootLayout({
